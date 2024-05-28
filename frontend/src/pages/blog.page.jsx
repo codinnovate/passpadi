bcbe43d386ac76f0579c20f3141d69b397fd8d53
codinnovate <kidscantech1@gmail.com>
2024-05-28 23:15:06 +0100

    fixed detail page

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -77,7 +77,7 @@ const BlogPage = () => {
                 loading ? <Loader />
                     : 
                     <BlogContext.Provider value={{ blog, setBlog, isLikedByUser, setLikedByUser, commentsWrapper, setCommentsWrapper, totalParentCommentsLoaded, setTotalParentCommentsLoaded }}>
-                    <CommentsContainer />
+                    {/* <CommentsContainer /> */}
                     <div className='max-w-[900px] center py-10 max-lg:px-[5vw] '>
                         <img src={banner} className='aspect-video ' />
                         <div className='mt-12'>
